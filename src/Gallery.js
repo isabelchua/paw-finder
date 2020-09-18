e7758f65e70ad06d6821e524836ab61b832d8721
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-17 19:18:54 -0700

    fetching images

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function Gallery({ dog }) {
 	return (
-		<div>
-			<img className="dog-pic" alt={dog + " pic"} src={dog} />
-		</div>
+		<>
+			<h1>Gallery</h1>
+			<div className="dog-contain">
+				<img className="dog-pic" alt={dog + " pic"} src={dog} />
+			</div>
+		</>
 	);
 }
 
